566d0d8ca3cf3dc87889c0c9c4ae9c2134402737
mywebsiteplanners <75284988+mywebsiteplanners@users.noreply.github.com>
2024-07-28 13:17:51 -0400

    adding route to fetch single property

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
 async function fetchProperties() {
   try {
     // handle the case where the domain is not available yet
@@ -19,4 +20,24 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// fetch single properties
+async function fetchProperty(id) {
+  try {
+    // handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
