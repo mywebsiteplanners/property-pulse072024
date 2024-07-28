e4f420630a62f1a344c8648831bdb09449c51bc7
mywebsiteplanners <75284988+mywebsiteplanners@users.noreply.github.com>
2024-07-28 12:39:27 -0400

    reading properties from db

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,9 +1,12 @@
 import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
 export const GET = async (request) => {
   try {
     await connectDB();
-    return new Response(JSON.stringify({ message: "hello world" }), {
+    const properties = await Property.find({});
+
+    return new Response(JSON.stringify(properties), {
       status: 200,
     });
   } catch (error) {
