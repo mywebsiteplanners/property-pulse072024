67b069e059249cf307342bfd8736bb5b94c77b6f
mywebsiteplanners <75284988+mywebsiteplanners@users.noreply.github.com>
2024-07-28 21:27:59 -0400

    adding login ability and storing user into data base

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -18,15 +20,30 @@ export const authOptions = {
     // invoked on successful signin
     async signIn({ profile }) {
       // 1. connect to db
+      await connectDB();
       // 2. check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3. if not, then add user to db
+      if (!userExists) {
+        // truncate user name if too long
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. return true to allow sign in
+      return true;
     },
     // modifies the session object
     async session({ session }) {
       // 1. get user from db
+      const user = await User.findOne({ email: session.user.email });
       // 2. assign the user id to the session
+      session.user.id = user._id.toString();
       // 3. return session
+      return session;
     },
   },
 };
