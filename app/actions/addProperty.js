827e894d68fb003e4a5c9bfd5eab4a5752983204
mywebsiteplanners <75284988+mywebsiteplanners@users.noreply.github.com>
2024-08-02 18:00:47 -0400

    updating addProperty action to read all form values into an object

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,7 +1,42 @@
 "use server";
 
 async function addProperty(formData) {
-  console.log("addproperty action", formData.get("name"));
+  // access all values from amenities and images, we need to use getAll
+  const amenities = formData.getAll("amenities");
+  // images need extra work, these are collection of objects
+  const images = formData
+    .getAll("images")
+    .filter((image) => image.name !== "")
+    .map((image) => image.name);
+
+  const propertyData = {
+    type: formData.get("type"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    location: {
+      street: formData.get("location.street"),
+      city: formData.get("location.city"),
+      state: formData.get("location.state"),
+      zipcode: formData.get("location.zipcode"),
+    },
+    beds: formData.get("beds"),
+    baths: formData.get("baths"),
+    square_feet: formData.get("square_feet"),
+    amenities,
+    rates: {
+      nightly: formData.get("rates.nightly"),
+      weekly: formData.get("rates.weekly"),
+      monthly: formData.get("rates.monthly"),
+    },
+    seller_info: {
+      name: formData.get("seller_info.name"),
+      email: formData.get("seller_info.email"),
+      phone: formData.get("seller_info.phone"),
+    },
+    images,
+  };
+
+  console.log(propertyData);
 }
 
 export default addProperty;
