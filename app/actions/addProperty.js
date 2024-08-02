e97040a1ea56613295683210582310e4fdd02221
mywebsiteplanners <75284988+mywebsiteplanners@users.noreply.github.com>
2024-08-02 19:15:39 -0400

    updating add property action to store property to database

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,6 +1,22 @@
 "use server";
 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 async function addProperty(formData) {
+  // connect to DB
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+
   // access all values from amenities and images, we need to use getAll
   const amenities = formData.getAll("amenities");
   // images need extra work, these are collection of objects
@@ -10,6 +26,7 @@ async function addProperty(formData) {
     .map((image) => image.name);
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -36,7 +53,12 @@ async function addProperty(formData) {
     images,
   };
 
-  console.log(propertyData);
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
